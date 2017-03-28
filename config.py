b3e9ee5e3f578cd3abdea48f7d65951147981d42
Roger Howard <rogerhoward@mac.com>
2017-03-28 13:58:36 -0700

    Initial config.py

---
+++ config.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import os, sys
+
+
+# Root directory of the project
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Path to a writeable filesystem. Shouldn't need to change this.
+TEMP_DIR = '/tmp/'
+
+# Path to the directory for static files
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+
+#------------------------------------------------#
+# ENV VARS                                       #
+#------------------------------------------------#
+
+ENV = dict(os.environ)
+
+
+# Used for templates and other places where we need to prepend the
+# name of the Zappa stage to the URL when deployed, but not 
+# during local testing
+if ENV.get('STAGE'):
+    URL_PREFIX = '/{}'.format(ENV.get('STAGE'))
+else:
+    URL_PREFIX = ''
+
+# Make sure no AWS security credentials are in ENV
+if 'AWS_SECURITY_TOKEN' in ENV:
+    del ENV['AWS_SECURITY_TOKEN']
+if 'AWS_SECRET_ACCESS_KEY' in ENV:
+    del ENV['AWS_SECRET_ACCESS_KEY']
+
+
+#------------------------------------------------#
+# Context dict for passing to templates          #
+#------------------------------------------------#
+
+
+CONTEXT = {'URL_PREFIX': URL_PREFIX, 'ENV': ENV}
