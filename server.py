70d38576633c1babbe77a2752f05eafc81c5e94e
Roger Howard <rogerhoward@mac.com>
2017-03-28 13:58:43 -0700

    Initial server.py

---
+++ server.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+import os
+import flask
+import boto3
+import config
+
+try:
+    from cStringIO import StringIO
+except:
+    from StringIO import StringIO
+
+app = flask.Flask(__name__)
+
+
+#------------------------------------------------#
+#  Bot endpoint                                  #
+#------------------------------------------------#
+
+@app.route('/chat/')
+def chat():
+    """
+    Dual routes which support browsing a list of zoomable images on your S3 bucket,
+    and viewing each one in an Open Seadragon viewer.
+    """
+    return flask.jsonify({'hello': 'world'})
+
+
+#------------------------------------------------#
+# Supporting endpoints                           #
+#------------------------------------------------#
+
+
+@app.route('/info/')
+def info():
+    """
+    Route which returns all environment variables as a JSON object.
+    """
+    return flask.jsonify({'env': config.ENV})
+
+
+@app.route('/static/<path:filepath>')
+def serve_static(filepath):
+    """
+    Route for serving static assets directly, rather than using S3.
+    Used for CSS, JS and other assets needed for the application.
+    """
+    return flask.send_from_directory(config.STATIC_ROOT, filepath)
+
+
+#------------------------------------------------#
+#  Command line options                          #
+#------------------------------------------------#
+
+def run():
+    app.run(processes=3, host='0.0.0.0', port=5000, debug=True)
+
+
+if __name__ == '__main__':
+    run()
