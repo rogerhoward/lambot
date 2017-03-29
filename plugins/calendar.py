b464aed2b167cf217f0b13fb2f11702914099113
Roger Howard <rogerhoward@mac.com>
2017-03-29 10:42:22 -0700

    prepend with echo

--- plugins/calendar.py
+++ plugins/calendar.py
@@ -19,7 +19,7 @@ class Action(object):
 
     @property
     def response(self):
-        return {'text': self.payload.get('text'), 'response_type': self.response_type}
+        return {'text': 'echo:' + self.payload.get('text'), 'response_type': self.response_type}
 
     def respond(self):
         if self.response:
