cd1b6a048b0fc6a6842153f6f740012d848acc59
Roger Howard <rogerhoward@mac.com>
2017-03-29 16:18:20 -0700

    Fix calendar text concatenation

--- plugins/calendar.py
+++ plugins/calendar.py
@@ -22,5 +22,6 @@ class Action(object):
     #     return {'text': 'echo:' + self.payload.get('text'), 'response_type': self.response_type}
 
     def respond(self):
-        response = {'text': 'echo:' + self.payload.get('text'), 'response_type': self.response_type}
+        message = 'echo: {}'.format(self.payload.get('text'))
+        response = {'text': message, 'response_type': self.response_type}
         requests.post(self.payload.get('response_url'), data=response)
