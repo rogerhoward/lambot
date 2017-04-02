f328ffc50a4451d3605261545bce4b3cd6fe5f06
Roger Howard <rogerhoward@mac.com>
2017-04-02 12:10:38 -0700

    .

--- plugins/calendar.py
+++ plugins/calendar.py
@@ -12,6 +12,7 @@ class Action(SimpleAction):
     feed = 'https://www.meetup.com/code-and-coffee-long-beach/events/atom/'
 
     def __init__(self, payload):
+        self.payload = payload
         print('preparing to load calendar...')
         if self.in_channel() and self.check():
             # try:
