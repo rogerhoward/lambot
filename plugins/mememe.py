754bae92933f3b1445b25dd05bf07f68a79ba6e1
Roger Howard <rogerhoward@mac.com>
2017-04-02 11:48:38 -0700

    Disable sample plugins

--- plugins/mememe.py
+++ plugins/mememe.py
@@ -1,54 +1,54 @@
-import requests
-import json
-import random
+# import requests
+# import json
+# import random
 
 
-class Action(object):
-    channels = ['bot', ]
-    payload = None
-    response_type = 'in_channel'
+# class Action(object):
+#     channels = ['bot', ]
+#     payload = None
+#     response_type = 'in_channel'
 
-    def __init__(self, payload):
-        print('loading mememe plugin with', payload)
-        # Payload format documented at https://api.slack.com/slash-commands#how_do_commands_work
-        self.payload = payload
+#     def __init__(self, payload):
+#         print('loading mememe plugin with', payload)
+#         # Payload format documented at https://api.slack.com/slash-commands#how_do_commands_work
+#         self.payload = payload
 
         
-        if self.channels and self.payload['channel'] in self.channels:
+#         if self.channels and self.payload['channel'] in self.channels:
 
-            try:
-                if self.payload.get('text')[0:6].lower() == 'mememe':
-                    self.respond()
-            except:
-                print('plugin {} failed. WTF.'.format(self.info['name']))
+#             try:
+#                 if self.payload.get('text')[0:6].lower() == 'mememe':
+#                     self.respond()
+#             except:
+#                 print('plugin {} failed. WTF.'.format(self.info['name']))
 
-    @property
-    def info(self):
-        return {'name': 'mememe', 
-                'title': 'Meme Me!', 
-                'description': 'Get a random meme - meme me.', 
-                'version': 1.0}
+#     @property
+#     def info(self):
+#         return {'name': 'mememe', 
+#                 'title': 'Meme Me!', 
+#                 'description': 'Get a random meme - meme me.', 
+#                 'version': 1.0}
 
-    def respond(self):
-        r = requests.get('https://api.imgflip.com/get_memes')
-        memes = r.json().get('data', {}).get('memes', [])
-        if len(memes) > 0:
-            random_meme = random.choice(memes)
+#     def respond(self):
+#         r = requests.get('https://api.imgflip.com/get_memes')
+#         memes = r.json().get('data', {}).get('memes', [])
+#         if len(memes) > 0:
+#             random_meme = random.choice(memes)
 
-            if self.payload.get('text') == 'mememe':
-                meme_attachment = {'image_url': random_meme['url'], 'fallback': random_meme['name'], 'title': random_meme['name']}
-            else:
-                caption = self.payload.get('text')[7:]
-                custom_meme = requests.post('https://api.imgflip.com/caption_image', data = {'template_id': random_meme['id'], 'username': 'imgflip_hubot', 'password': 'imgflip_hubot', 'text0': caption}).json()
-
-
-                meme_attachment = {'image_url': custom_meme['data']['url'], 'fallback': random_meme['name'], 'title': random_meme['name']}
+#             if self.payload.get('text') == 'mememe':
+#                 meme_attachment = {'image_url': random_meme['url'], 'fallback': random_meme['name'], 'title': random_meme['name']}
+#             else:
+#                 caption = self.payload.get('text')[7:]
+#                 custom_meme = requests.post('https://api.imgflip.com/caption_image', data = {'template_id': random_meme['id'], 'username': 'imgflip_hubot', 'password': 'imgflip_hubot', 'text0': caption}).json()
 
 
+#                 meme_attachment = {'image_url': custom_meme['data']['url'], 'fallback': random_meme['name'], 'title': random_meme['name']}
 
 
-            url = self.payload.get('response_url')
 
-            if url:
-                response_payload = {'text': 'Your hot, fresh meme:', 'attachments': [meme_attachment], 'response_type': self.response_type}
-                requests.post(url, json=response_payload)
+
+#             url = self.payload.get('response_url')
+
+#             if url:
+#                 response_payload = {'text': 'Your hot, fresh meme:', 'attachments': [meme_attachment], 'response_type': self.response_type}
+#                 requests.post(url, json=response_payload)
--- plugins/voice.py
+++ plugins/voice.py
@@ -1,29 +1,29 @@
-import requests
-import json
+# import requests
+# import json
 
 
-class Action(object):
-    channels = ['bot', ]
-    payload = None
-    response_type = 'ephemeral'
+# class Action(object):
+#     channels = ['bot', ]
+#     payload = None
+#     response_type = 'ephemeral'
 
-    def __init__(self, payload):
-        print('loading voice with', payload)
-        self.payload = payload
+#     def __init__(self, payload):
+#         print('loading voice with', payload)
+#         self.payload = payload
 
-        if self.channels and self.payload['channel'] in self.channels:
-            try:
-                self.respond()
-            except:
-                print('plugin {} failed. WTF.'.format(self.info['name']))
+#         if self.channels and self.payload['channel'] in self.channels:
+#             try:
+#                 self.respond()
+#             except:
+#                 print('plugin {} failed. WTF.'.format(self.info['name']))
 
-    @property
-    def info(self):
-        return {'name': 'voice', 
-                'title': 'Voice Synth', 
-                'description': 'A text-to-speech synthesizer.', 
-                'version': 1.0}
+#     @property
+#     def info(self):
+#         return {'name': 'voice', 
+#                 'title': 'Voice Synth', 
+#                 'description': 'A text-to-speech synthesizer.', 
+#                 'version': 1.0}
 
-    def respond(self):
-        pass
+#     def respond(self):
+#         pass
 
