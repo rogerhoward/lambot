671df09d3add3f71ed715ddebf8380f06b0e50ac
Roger Howard <rogerhoward@mac.com>
2017-04-02 12:16:02 -0700

    At timedate to venue

--- plugins/calendar.py
+++ plugins/calendar.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import datetime
 from action import SimpleAction
 import config
 
@@ -42,7 +43,8 @@ class Action(SimpleAction):
         if len(meetups) > 0:
             next_meet = meetups[0]
             venue = next_meet['venue']
-            message = 'The next Meetup is at {name}, located at {address_1}, {city}, {state}'.format(**venue)
+            venue['timedate'] = datetime.datetime.fromtimestamp(next_meet['time']).strftime('%c')
+            message = 'The next Meetup is {timedate} at {name}, located at {address_1}, {city}, {state}'.format(**venue)
 
             location_escaped = '{address},+{city},+{state}'.format(**{'address': venue['address_1'].replace(' ', '+'), 'city': venue['city'].replace(' ', '+'), 'state': venue['state'].replace(' ', '+')})
             map_image = 'https://maps.googleapis.com/maps/api/staticmap?center={location}&zoom=15&scale=2&size=400x400&maptype=roadmap&format=png&visual_refresh=true&markers=size:mid%7Ccolor:0xff0000%7Clabel:1%7C{location}'.format(**{'location': location_escaped})
