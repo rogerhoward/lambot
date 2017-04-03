693c33f2d8624ed7248c3b07c61a13c637c91ae9
Roger Howard <rogerhoward@mac.com>
2017-04-03 07:55:06 -0700

    Add help plugin

---
+++ plugins/help.py
@@ -0,0 +1,30 @@
+import requests
+import json
+import datetime
+import arrow
+from action import SimpleAction
+import config
+
+class Action(SimpleAction):
+    name = 'help'
+    title = 'Lambot Help'
+    description = 'Lists available plugins and their descriptions.'
+    version = 0.1
+    
+
+    def in_channel(self):
+        return True
+
+    def check(self):
+        if self.text.lower = 'help':
+            print('help plugin activated..')
+            return True
+        else:
+            print('help plugin ignored...')
+            return False
+
+
+    def response(self):
+        # Create Slack response payload
+        response_payload = {'text': ', '.join(plugin_names), 'response_type': self.response_type}
+        return response_payload
