d1df064eebaad9f5fdb120cc0580e88606d4dd26
Roger Howard <rogerhoward@mac.com>
2017-04-03 22:39:00 -0700

    Updated finger plugin with listall command

--- plugins/finger.py
+++ plugins/finger.py
@@ -13,7 +13,7 @@ class Action(SimpleAction):
     description = 'Provies a simple service like the old unix finger command. Users can maintain a finger file - like a social media profile - and that others can read.'
     version = 0.1
     help_command = 'finger help'
-    help_string = '"/lambot finger $username" will show the finger file for the user whose name was provided.\n"/lambot finger update: $message" will update your finger file with the provided message.'
+    help_string = '"/lambot finger $username" will show the finger file for the user whose name was provided.\n"/lambot finger update: $message" will update your finger file with the provided message.\n"/lambot finger listall" will list all users with finger files.'
 
     channels = '*'
 
@@ -34,6 +34,22 @@ class Action(SimpleAction):
             finger_message = self.text[14:]
             self.dynamodb.put_item(TableName=self.name, Item={'username': {'S': self.user_name}, 'message': {'S': finger_message}})
             response_payload = {'text': 'your finger file has been updated', 'response_type': self.response_type}
+        
+        elif self.text == ('finger listall'):
+            usernames = []
+            response = self.dynamodb.scan(TableName=self.name, AttributesToGet=['username'])
+            data = response['Items']
+
+            while 'LastEvaluatedKey' in response:
+                response = self.dynamodb.scan(TableName=self.name, AttributesToGet=['username'], ExclusiveStartKey=response['LastEvaluatedKey'])
+                data.extend(response['Items'])
+
+            for item in data:
+                usernames.append(item['username']['S'])
+
+            message = 'users with finger files: {}'.format(', '.join(usernames)) 
+            response_payload = {'text': message, 'response_type': self.response_type}
+
         else:
             username = self.text[7:]
             rec = self.dynamodb.get_item(TableName=self.name, Key={'username':{'S': username}})
